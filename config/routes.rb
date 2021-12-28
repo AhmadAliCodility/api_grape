18ba8eaceed864f1e05e834f7c8ac69da98d9e00
AhmadAliCodility <ahmed.ali@codilitysolutions.com>
2021-12-28 12:57:48 +0500

    Song Crud Using Grape API

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
-  # mount MusicStore::Base => '/'
-  # mount MusicStore::V1::Song => '/'
-
+  # Todo: ==> rails grape:routes <==
   mount MusicStore::Base => '/'
+
   # mount Twitter::Api => '/'
   mount Converter::Currency => 'api'
   namespace 'api' do
